317f60f982c1d8c3a1847ae4591099ffce0bf229
Joshua Nixon <nixonjoshua98@gmail.com>
2020-01-15 20:50:20 +0000

    Added join date command

--- darkness/cogs/server_stats.py
+++ darkness/cogs/server_stats.py
@@ -12,3 +12,13 @@ class ServerStats(commands.Cog):
 	@commands.command(name="size")
 	async def size(self, ctx):
 		await ctx.send(f"The darkness has taken over ``{ctx.guild.member_count}`` accounts!")
+
+	@commands.command(name="joined")
+	async def joined(self, ctx):
+		joined_at = ctx.author.joined_at
+
+		date = joined_at.strftime("%B %d, %Y")
+
+		msg = f"``{ctx.author.display_name}`` joined on {date}"
+
+		await ctx.send(msg)
