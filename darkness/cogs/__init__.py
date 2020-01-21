559a63fbf1fa4d67d0a9ad149437cbe444d11ca8
Joshua Nixon <nixonjoshua98@gmail.com>
2020-01-21 15:55:25 +0000

    removed some cogs

--- darkness/cogs/__init__.py
+++ darkness/cogs/__init__.py
@@ -1,7 +1,5 @@
-from darkness.cogs.server_cog import Server
 from darkness.cogs.greetings_cog import Greetings
 from darkness.cogs.help_cog import Help
-from darkness.cogs.account_cog import Account
 from darkness.cogs.bot_cog import BotCog
 from darkness.cogs.activity_cog import Activity
 
@@ -9,8 +7,6 @@ from darkness.cogs.activity_cog import Activity
 cog_list = [
 	Activity,
 	Greetings,
-	Server,
 	Help,
 	BotCog,
-	Account
 ]
