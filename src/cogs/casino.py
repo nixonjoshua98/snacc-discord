e810689fcabdc2c7a8a88d241093ea69ca68d045
Joshua Nixon <nixonjoshua98@gmail.com>
2020-02-26 11:26:50 +0000

    Update casino.py

--- src/cogs/casino.py
+++ src/cogs/casino.py
@@ -14,8 +14,8 @@ class Casino(commands.Cog):
 		return await checks.in_game_room(ctx) and commands.guild_only()
 
 	@checks.has_minimum_coins("coins.json", 10)
-	#@commands.cooldown(1, 3, commands.BucketType.user)
-	@commands.command(name="spin", aliases=["sp"], help="Spin a slot machine")
+	@commands.cooldown(1, 3, commands.BucketType.user)
+	@commands.command(name="spin", aliases=["sp"], help="Slot machine [LOW RISK]")
 	async def spin(self, ctx):
 		machine = SpinMachine(ctx)
 
