a3bfe13c5dd170099338a41a724dbe27d0f62e2f
Joshua Nixon <nixonjoshua98@gmail.com>
2020-03-10 19:46:33 +0000

    Update greetings.py

--- src/cogs/greetings.py
+++ src/cogs/greetings.py
@@ -10,21 +10,22 @@ class Greetings(commands.Cog, name="greetings"):
 
 	@commands.Cog.listener()
 	async def on_member_join(self, member: discord.Member):
-		sys_channel = member.guild.system_channel
 
-		if sys_channel:
-			await sys_channel.send(f"Welcome {member.mention} to {member.guild.name}!")
+		# Send welcome message to the system channel (if available)
+		if member.guild.system_channel:
+			await member.guild.system_channel.send(f"Welcome {member.mention} to {member.guild.name}!")
 
+		# Get the initial role (if any)
 		with FileReader("server_settings.json") as f:
 			initial_role_id = f.get(member.guild.id, default_val={}).get("entry_role", None)
 
 		member_role = discord.utils.get(member.guild.roles, id=initial_role_id)
 
+		# Try to add the welcome role
 		if member_role is not None:
 			await member.add_roles(member_role, atomic=True)
 
 	@commands.Cog.listener()
 	async def on_member_remove(self, member):
-		sys_channel = member.guild.system_channel
-
-		await sys_channel.send(f"**{member.display_name}** has left the server :frowning:")
+		if member.guild.system_channel:
+			await member.guild.system_channel.send(f"**{member.display_name}** has left the server :frowning:")
