7eaee4818286493f9326e70741c829b421caa3e7
Joshua Nixon <nixonjoshua98@gmail.com>
2020-03-10 18:41:21 +0000

    Update myjson.py

--- src/common/myjson.py
+++ src/common/myjson.py
@@ -12,6 +12,7 @@ JSON_URL_LOOKUP = {
 	"game_stats.json": "https://api.myjson.com/bins/1d98rq",
 	"coins.json": "https://api.myjson.com/bins/fb9yk",
 	"pet_stats.json": "https://api.myjson.com/bins/leyri",
+	"server_settings.json": "https://api.myjson.com/bins/f4aku"
 }
 
 headers = {
