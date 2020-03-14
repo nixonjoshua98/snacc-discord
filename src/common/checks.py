385f21e343a38e006a68299de24f395718f7720a
Joshua Nixon <nixonjoshua98@gmail.com>
2020-03-14 19:17:25 +0000

    Fixed

--- src/common/checks.py
+++ src/common/checks.py
@@ -7,7 +7,6 @@ from discord.ext import commands
 from discord.ext.commands import CommandError
 
 from src.common import FileReader
-from src.common.constants import SNACCMAN_ID
 
 
 def requires_channel_tag(tag):
@@ -36,7 +35,7 @@ def percent_chance_to_run(percentage, fail_message):
 
 
 async def is_server_owner(ctx):
-	if ctx.author.id not in (ctx.guild.owner.id, SNACCMAN_ID):
+	if ctx.author.id != ctx.guild.owner.id:
 		raise CommandError(f"**{ctx.author.display_name}**, you do not have access to this command")
 
 	return True
@@ -51,7 +50,7 @@ async def has_member_role(ctx):
 	if member_role is None:
 		raise CommandError("Member role is invalid or has not been set")
 
-	elif member_role not in ctx.author.roles and ctx.author.id != SNACCMAN_ID:
+	elif member_role not in ctx.author.roles:
 		raise CommandError(f"**{ctx.author.display_name}** you need the **{member_role.name}** role.")
 
 	return True
--- src/common/constants.py
+++ src/common/constants.py
@@ -1,7 +1,5 @@
 import os
 
-SNACCMAN_ID = 281171949298647041
-
 RESOURCES_DIR = os.path.join(os.getcwd(), "src", "resources")
 
 ALL_CHANNEL_TAGS = ("abo", "game")
