3567feeb2085a42dc576bc1bc071f04eaeda3136
Joshua Nixon <nixonjoshua98@gmail.com>
2020-05-06 23:35:19 +0100

    d

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -11,12 +11,10 @@ class Misc(commands.Cog):
 
     @commands.command(name="ban")
     async def ban(self, ctx: commands.Context, user: DiscordUser()):
-        """ Ban a user from the server (give it a go). """
+        """ Totally ban a user from this server. """
 
         try:
-            username = user.display_name.replace('[BANNED] ', '')
-
-            await user.edit(nick=f"[BANNED] {username[0:20]}")
+            await user.edit(nick=f"[BANNED] {user.display_name[0:20]}")
 
             await ctx.send("I have banned them successfully.")
 
@@ -25,6 +23,21 @@ class Misc(commands.Cog):
 
         except discord.HTTPException:
             await ctx.send("I have failed to ban this user")
+
+    @commands.command(name="unban")
+    async def unban(self, ctx: commands.Context, user: DiscordUser()):
+        """ Unban a user from this server. """
+
+        try:
+            await user.edit(nick=user.display_name.replace("[BANNED]", "")
+
+            await ctx.send("I have unbanned them successfully.")
+
+        except discord.Forbidden:
+            await ctx.send("I failed. The user is too powerful for me to unban!")
+
+        except discord.HTTPException:
+            await ctx.send("I have failed to unban this user")
 
 
 def setup(bot):
