3845c5b3894ab160bbd79b004e9a29d4e23dc2c3
Joshua Nixon <nixonjoshua98@gmail.com>
2020-05-20 17:30:30 +0100

    mhm

--- snacc/classes/bot.py
+++ snacc/classes/bot.py
@@ -27,10 +27,11 @@ class SnaccBot(commands.Bot):
         print("OK")
 
     def load_extensions(self):
-        print(os.listdir("."))
         for root, dirs, files in os.walk(".\\snacc\\exts"):
             for f in files:
                 path = os.path.join(root, f)
+
+                print(path)
 
                 if not f.startswith("__") and not f.endswith("__") and f.endswith(".py"):
                     ext = path[2:].replace("\\", ".")[:-3]
