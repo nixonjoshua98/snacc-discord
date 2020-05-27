10896f18b8700b408b43c05ea697da8febf7ba4e
Aaryan Sarawgi <43273163+aaryan-sarawgi1@users.noreply.github.com>
2020-05-27 07:44:59 +0530

    fixed stuff
    fixed movies.txt

--- stuff_removalprototype.py
+++
@@ -1,9 +0,0 @@
-with open('./snacc/data/hangman/movies.txt') as  f:
-	li=[]
-	for line in f:
-		if line!='\n':
-			li.append(line)
-f=open('./snacc/data/hangman/movies.txt','w')
-for line in li:
-	f.write(line)
-print("Done")
