d5d3b6ba9764c833a6699c3c628c03af004db90a
joshuanixon <nixonjoshua98@gmail.com>
2020-07-16 18:05:45 +0100

    Updated treasure event

--- src/exts/empire/empireevents.py
+++ src/exts/empire/empireevents.py
@@ -6,6 +6,33 @@ from . import empireunits as units
 
 
 async def ambush_event(ctx):
+	units_lost = _lose_units_event(ctx)
+
+	s = []
+	for unit, n in units_lost:
+		s.append(f"{n}x {unit.display_name}")
+
+		await EmpireM.sub_unit(ctx.bot.pool, ctx.author.id, unit, n)
+
+	await ctx.send(f"You was ambushed and lost **{', '.join(s) or 'nothing'}**!")
+
+
+async def treaure_event(ctx):
+	items = ("tiny ruby", "pink diamond", "small emerald", "holy sword", "demon sword", "dead slave")
+
+	async with ctx.bot.pool.acquire() as con:
+		bank = await con.fetchrow(BankM.SELECT_ROW, ctx.author.id)
+
+		money = bank[BankM.MONEY]
+
+		money_gained = random.randint(max(250, money // 50), max(1_000, money // 25))
+
+		await ctx.bot.pool.execute(BankM.ADD_MONEY, ctx.author.id, money_gained)
+
+	await ctx.send(f"You found a **{random.choice(items)}** which sold for **${money_gained}**")
+
+
+async def _lose_units_event(ctx):
 	async with ctx.bot.pool.acquire() as con:
 		empire = await con.fetchrow(EmpireM.SELECT_ROW, ctx.author.id)
 
@@ -27,23 +54,4 @@ async def ambush_event(ctx):
 
 				target_units.remove(unit)
 
-		s = []
-		for unit, n in units_lost:
-			s.append(f"{n}x {unit.display_name}")
-
-			await EmpireM.sub_unit(con, ctx.author.id, unit, n)
-
-	await ctx.send(f"You was ambushed and lost **{', '.join(s) or 'nothing'}**!")
-
-
-async def treaure_event(ctx):
-	async with ctx.bot.pool.acquire() as con:
-		bank = await con.fetchrow(BankM.SELECT_ROW, ctx.author.id)
-
-		money = bank[BankM.MONEY]
-
-		money_gained = random.randint(max(250, money // 50), max(1_000, money // 25))
-
-		await ctx.bot.pool.execute(BankM.ADD_MONEY, ctx.author.id, money_gained)
-
-	await ctx.send(f"You found some treasure while patrolling which sold for **${money_gained}**")
+	return units_lost
