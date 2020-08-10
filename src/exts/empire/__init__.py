a1008952ab8341506bcb5334c4abd9acc0076552
joshuanixon <nixonjoshua98@gmail.com>
2020-08-10 16:17:34 +0100

    Updated win chance

--- src/exts/empire/__init__.py
+++ src/exts/empire/__init__.py
@@ -46,7 +46,7 @@ class Empire(commands.Cog):
 
 	@staticmethod
 	def get_win_chance(atk_power, def_power):
-		return max(0.15, min(0.85, 0.2 + ((atk_power / max(1, def_power)) / 2.0)))
+		return max(0.15, min(0.85, ((atk_power / max(1, def_power)) / 2.0)))
 
 	@staticmethod
 	async def get_hourly_income(con, user):
