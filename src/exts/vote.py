e9f68769a7f825cdf5625e49c42e0a5bf9c8c0d2
joshuanixon <nixonjoshua98@gmail.com>
2020-09-27 00:00:05 +0100

    last attempt

--- src/exts/vote.py
+++ src/exts/vote.py
@@ -6,32 +6,6 @@ from discord.ext import commands
 
 from src.common import SupportServer
 
-from aiohttp import web
-
-
-async def webhook(self):
-	async def vote_handler(request):
-		print(request)
-
-		req_auth = request.headers.get('Authorization')
-		if self.webhook_auth == req_auth:
-			data = await request.json()
-			if data.get('type') == 'upvote':
-				event_name = 'dbl_vote'
-			elif data.get('type') == 'test':
-				event_name = 'dbl_test'
-			self.bot.dispatch(event_name, data)
-			return web.Response()
-		else:
-			return web.Response(status=401)
-
-	app = web.Application(loop=self.loop)
-	app.router.add_post(self.webhook_path, vote_handler)
-	runner = web.AppRunner(app)
-	await runner.setup()
-	self._webserver = web.TCPSite(runner, '0.0.0.0', self.webhook_port)
-	await self._webserver.start()
-
 
 class Vote(commands.Cog):
 
@@ -43,9 +17,7 @@ class Vote(commands.Cog):
 	@commands.Cog.listener("on_startup")
 	async def on_startup(self):
 		if (token := os.getenv("DBL_TOKEN")) not in (None, "TOKEN", "VALUE", "", " "):
-			dbl.DBLClient.webhook = webhook
-
-			self.dbl = dbl.DBLClient(self.bot, token, autopost=True, webhook_auth="snacc")
+			self.dbl = dbl.DBLClient(self.bot, token, autopost=True, port=10010, webhook_auth="snacc")
 
 			print("Created DBL webhook")
 
@@ -55,27 +27,23 @@ class Vote(commands.Cog):
 
 	@commands.Cog.listener(name="on_dbl_vote")
 	async def on_dbl_vote(self, data):
-		print(data)
+		user_id = data["user"]
 
-		if data["type"] == "upvote" and data["bot"] == self.bot.user.id:
+		user = self.bot.get_user(user_id)
 
-			user_id = data["user"]
+		if user is not None:
+			support_server = self.bot.get_guild(SupportServer.ID)
 
-			user = self.bot.get_user(user_id)
+			try:
+				await user.send("Thank you for voting for me! :heart:")
 
-			if user is not None:
-				support_server = self.bot.get_guild(SupportServer.ID)
+				member = support_server.get_member(user_id)
 
-				try:
-					await user.send("Thank you for voting for me! :heart:")
+				if member is None:
+					await user.send(f"psst...you can join our support server here {SupportServer.LINK}")
 
-					member = support_server.get_member(user_id)
-
-					if member is None:
-						await user.send(f"psst...you can join our support server here {SupportServer.LINK}")
-
-				except (discord.Forbidden, discord.HTTPException):
-					""" Failed """
+			except (discord.Forbidden, discord.HTTPException):
+				""" Failed """
 
 
 def setup(bot):
