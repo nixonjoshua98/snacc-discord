1ba99395c94c5322efdc9fe257e498d93ccd0653
joshuanixon <nixonjoshua98@gmail.com>
2020-09-27 10:54:03 +0100

    minor

--- src/structs/mongoclient.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from motor.motor_asyncio import AsyncIOMotorClient
-
-
-class MongoClient(AsyncIOMotorClient):
-	def __init__(self):
-		super(MongoClient, self).__init__(os.getenv("MONGO_CON_STR"))
