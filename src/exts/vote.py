b79342a18c17aeb9f3de772ea50dabaaae2c1c40
joshuanixon <nixonjoshua98@gmail.com>
2020-09-28 00:15:31 +0100

    minor

--- src/exts/vote.py
+++ src/exts/vote.py
@@ -1,58 +1,12 @@
 import dbl
 import os
-import httpx
 import discord
-import asyncio
-
-from aiohttp import web
 
 from discord.ext import commands
 
 from src import utils
 
 from src.common import SupportServer
-
-
-class DiscordBotList:
-	def __init__(self, bot, *, webhook_path, webhook_auth, webhook_port):
-		self.bot = bot
-
-		self.webhook_path = webhook_path
-		self.webhook_auth = webhook_auth
-		self.webhook_port = webhook_port
-
-		asyncio.create_task(self.webhook())
-
-		asyncio.create_task(self.auto_post())
-
-	async def webhook(self):
-		async def vote_handler(request):
-			print(request)
-
-		app = web.Application(loop=self.bot.loop)
-
-		app.router.add_post(self.webhook_path, vote_handler)
-
-		runner = web.AppRunner(app)
-
-		await runner.setup()
-
-		webserver = web.TCPSite(runner, '0.0.0.0', self.webhook_port)
-
-		await webserver.start()
-
-	async def auto_post(self):
-		url = f"https://discordbotlist.com/api/v1/bots/{self.bot.user.id}/stats"
-
-		headers = {"Authorization": self.webhook_auth}
-
-		while not self.bot.is_closed():
-			body = {"users": len(self.bot.users), "guilds": len(self.bot.guilds)}
-
-			async with httpx.AsyncClient() as client:
-				r = await client.post(url, headers=headers, data=body)
-
-			await asyncio.sleep(1_800)
 
 
 class Vote(commands.Cog):
@@ -63,6 +17,7 @@ class Vote(commands.Cog):
 	@commands.Cog.listener("on_startup")
 	async def on_startup(self):
 		if (token := os.getenv("DBL_TOKEN")) not in (None, "TOKEN", "VALUE", "", " "):
+
 			auth = os.environ["VOTE_AUTH"]
 
 			dbl.DBLClient(
@@ -74,13 +29,13 @@ class Vote(commands.Cog):
 				webhook_auth=auth
 			)
 
-			DiscordBotList(self.bot, webhook_path="/dblhook", webhook_auth=auth, webhook_port=5001)
-
 			print("Created vote clients")
 
 	@commands.Cog.listener(name="on_dbl_test")
 	async def on_dbl_test(self, data):
 		print(data)
+
+		await self.on_dbl_vote(data)
 
 	@commands.Cog.listener(name="on_dbl_vote")
 	async def on_dbl_vote(self, data):
